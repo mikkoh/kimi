405d5782567041a485c4e5a6e885ca31cf02fbc9
Mikko Haapoja <me@mikkoh.com>
2015-11-09 13:14:56 -0500

    Added a check so that you cannot step unless current and target return back a duration

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ kimi.prototype = {
 
   step: function(delta) {
 
-    if(this.currentPath.length || this.targetState) {
+    if((this.currentPath.length || this.targetState) && this.directions.fromTo(this.currentState, this.targetState) !== undefined) {
 
       var to = this.currentPath[ 0 ];
       var isReversing = this.allowReverse && (this.currentState === to || ( this.targetState && to && to !== this.targetState ));
